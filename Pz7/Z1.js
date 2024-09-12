cfe8b5bc2e06821ccbe8d167729063207d986525
StrelnikovaAnastasia <astrelnikova1901@mail.com>
2024-09-12 23:09:29 +0300

    Pz7 folder Z1

---
+++ Pz7/Z1.js
@@ -0,0 +1,26 @@
+function showtype(arg) {
+    var type = typeof (arg);
+
+    switch (type) {
+        case 'null':
+            console.log('Отсутствие значения')
+            break
+        case 'string':
+            console.log('Строка')
+            break
+        case 'object':
+            console.log('Объект')
+            break
+        case 'number':
+            console.log('Число')
+            break
+        case 'boolean':
+            console.log('Логический тип')
+            break
+        case 'function':
+            console.log('Функция')
+            break
+    }
+}
+
+showtype('hello');
