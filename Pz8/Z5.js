bd6330923385b83102cea54987c873d11a729016
StrelnikovaAnastasia <astrelnikova1901@mail.com>
2024-09-14 22:12:54 +0300

    Pz8 folder Z5

---
+++ Pz8/Z5.js
@@ -0,0 +1,31 @@
+var FirstClass = {
+    children: [
+        { name: "Ваня", age: 7 },
+        { name: "Ира", age: 7 },
+        { name: "Лиза", age: 6 },
+        { name: "Андрей", age: 7 },
+        { name: "Саша", age: 8 },
+        { name: "Женя", age: 8 },
+        { name: "Кеша", age: 6 },
+        { name: "Леша", age: 6 }
+
+    ],
+
+    get property() {
+        const compareFastAlf = (a, b) => String(a.name).localeCompare(b.name);
+        const compareFastVozr = (a, b) => a.age - b.age;
+        this.children.sort(compareFastAlf);
+        this.children.sort(compareFastVozr);
+        return this.children;
+    },
+
+    set property({ name, age }) {
+        let nameUp = name.toLowerCase();
+        nameUp = nameUp[0].toUpperCase() + nameUp.slice(1);
+        this.children.push({ name: nameUp, age: age });
+    }
+}
+
+FirstClass.property = { name: "наСтя", age: 8 };
+console.log(FirstClass.children);
+console.log(FirstClass.property);
