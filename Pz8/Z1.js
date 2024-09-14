d82368944bab210ee9fb8a8fcbbd86924f5c3ba5
StrelnikovaAnastasia <astrelnikova1901@mail.com>
2024-09-14 17:15:27 +0300

    Pz8 folder Z1

---
+++ Pz8/Z1.js
@@ -0,0 +1,35 @@
+Queue = {
+    commands: [
+        { id: "1-command", command: "go" },
+        { id: "2-command", command: "rotate" },
+        { id: "3-command", command: "push" },
+        { id: "4-command", command: "pop" },
+        { id: "5-command", command: "reverse" }
+    ],
+    get property() {
+         let el1 = this.commands.shift().command;
+         this.commands.forEach((item,index) => {
+         item.id = String((index+1) + '-command');
+        })
+        return el1;
+    },
+
+    set property(command) {
+        const obj = {id: String((this.commands.length+1) + "-command"), command: command};
+        this.commands.push(obj);
+    },
+
+    showQueue: function () {
+        str = "abcdefghijklmnopqrstuvwxyz"
+        this.commands.forEach((item,index) => {
+            console.log(str[index], ". ", item.command);
+        })
+
+    }
+
+}
+
+console.log(Queue.property);
+Queue.property = "shift";
+console.log(Queue.commands);
+Queue.showQueue();
