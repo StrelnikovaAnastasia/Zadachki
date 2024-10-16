c7903bba7754f6fbdb34a37a12b651006e290021
StrelnikovaAnastasia <astrelnikova1901@mail.com>
2024-10-16 23:04:37 +0300

    fix

--- Pz11/Z5/Z5.js
+++ Pz11/Z5/Z5.js
@@ -6,45 +6,47 @@ const form = $.createElement("form");
 const header = $.createElement("header");
 const h2 = $.createElement("h2");
 const titles = $.createElement("div");
-const titles__item_name = $.createElement("div");
+const titles__itemName = $.createElement("div");
 const nameP = $.createElement("p");
-const titles__item_lastname = $.createElement("div");
+const titles__itemLastname = $.createElement("div");
 const lastnameP = $.createElement("p");
-const titles__item_group = $.createElement("div");
+const titles__itemGroup = $.createElement("div");
 const groupP = $.createElement("p");
-const p_1 = $.createElement("p");
+const p1 = $.createElement("p");
 const input = $.createElement("div");
-const label_input__item_name = $.createElement("label");
-const input__item_name = $.createElement("input");
-const label_input__item_lastname = $.createElement("label");
-const input__item_lastname = $.createElement("input");
-const label_input__item_group = $.createElement("label");
-const input__item_group = $.createElement("input");
-const p_2 = $.createElement("p");
-const label_button = $.createElement("label");
+const labelInput__itemName = $.createElement("label");
+const input__itemName = $.createElement("input");
+const labelInput__itemLastname = $.createElement("label");
+const input__itemLastname = $.createElement("input");
+const labelInput__itemGroup = $.createElement("label");
+const input__itemGroup = $.createElement("input");
+const p2 = $.createElement("p");
+const btn = $.createElement("div");
+const labelButton = $.createElement("label");
 const button = $.createElement("button");
 
 divform.appendChild(form);
 form.appendChild(header);
+form.appendChild(p1);
 form.appendChild(titles);
-form.appendChild(p_1);
 form.appendChild(input);
-form.appendChild(p_2);
-form.appendChild(label_button);
+form.appendChild(p2);
+form.appendChild(btn);
+btn.appendChild(labelButton);
 header.appendChild(h2);
-titles.appendChild(titles__item_name);
-titles.appendChild(titles__item_lastname);
-titles.appendChild(titles__item_group);
-input.appendChild(label_input__item_name);
-input.appendChild(label_input__item_lastname);
-input.appendChild(label_input__item_group);
-label_button.appendChild(button);
-titles__item_name.appendChild(nameP);
-titles__item_lastname.appendChild(lastnameP);
-titles__item_group.appendChild(groupP);
-label_input__item_name.appendChild(input__item_name);
-label_input__item_lastname.appendChild(input__item_lastname);
-label_input__item_group.appendChild(input__item_group);
+titles.appendChild(titles__itemName);
+titles.appendChild(titles__itemLastname);
+titles.appendChild(titles__itemGroup);
+input.appendChild(labelInput__itemName);
+input.appendChild(labelInput__itemLastname);
+input.appendChild(labelInput__itemGroup);
+labelButton.appendChild(button);
+titles__itemName.appendChild(nameP);
+titles__itemLastname.appendChild(lastnameP);
+titles__itemGroup.appendChild(groupP);
+labelInput__itemName.appendChild(input__itemName);
+labelInput__itemLastname.appendChild(input__itemLastname);
+labelInput__itemGroup.appendChild(input__itemGroup);
 
 $.body.appendChild(divform);
 
@@ -58,138 +60,122 @@ divform.id = "div_form";
 form.id = "form";
 header.classList.add("header");
 titles.classList.add("titles");
-titles__item_name.classList.add("titles__item", "otstup");
-titles__item_lastname.classList.add("titles__item", "otstup");
-titles__item_group.classList.add("titles__item");
+titles__itemName.classList.add("titles__item", "otstup");
+titles__itemLastname.classList.add("titles__item", "otstup");
+titles__itemGroup.classList.add("titles__item");
 input.classList.add("input");
-input__item_name.classList.add("input__item");
-input__item_name.id = "input_1";
-input__item_lastname.classList.add("input__item");
-input__item_lastname.id = "input_2";
-input__item_group.classList.add("input__item");
-input__item_group.id = "input_3";
+input__itemName.classList.add("input__item");
+input__itemName.id = "input_1";
+input__itemLastname.classList.add("input__item");
+input__itemLastname.id = "input_2";
+input__itemGroup.classList.add("input__item");
+input__itemGroup.id = "input_3";
+btn.classList.add("btns");
 button.classList.add("button");
 
 const result = $.createElement("form");
-const titles_result = $.createElement("div");
-const titles_result__item_name = $.createElement("div");
-const titles_result__item_lastname = $.createElement("div");
-const titles_result__item_group = $.createElement("div");
-const titles_result__item_result = $.createElement("div");
-const titles_result__item_answer1 = $.createElement("div");
-const titles_result__item_answer2 = $.createElement("div");
-const titles_result__item_answer3 = $.createElement("div");
-const titles_result__item_answer4 = $.createElement("div");
-const titles_result__item_answer5 = $.createElement("div");
+const titlesResult = $.createElement("div");
+const titlesResult__itemName = $.createElement("div");
+const titlesResult__itemLastname = $.createElement("div");
+const titlesResult__itemGroup = $.createElement("div");
+const titlesResult__itemResult = $.createElement("div");
+const titlesResult__itemAnswer1 = $.createElement("div");
+const titlesResult__itemAnswer2 = $.createElement("div");
+const titlesResult__itemAnswer3 = $.createElement("div");
+const titlesResult__itemAnswer4 = $.createElement("div");
+const titlesResult__itemAnswer5 = $.createElement("div");
 
 divform.appendChild(result);
-result.appendChild(titles_result);
-titles_result.appendChild(titles_result__item_name);
-titles_result.appendChild(titles_result__item_lastname);
-titles_result.appendChild(titles_result__item_group);
-titles_result.appendChild(titles_result__item_result);
-titles_result.appendChild(titles_result__item_answer1);
-titles_result.appendChild(titles_result__item_answer2);
-titles_result.appendChild(titles_result__item_answer3);
-titles_result.appendChild(titles_result__item_answer4);
-titles_result.appendChild(titles_result__item_answer5);
+result.appendChild(titlesResult);
+titlesResult.appendChild(titlesResult__itemName);
+titlesResult.appendChild(titlesResult__itemLastname);
+titlesResult.appendChild(titlesResult__itemGroup);
+titlesResult.appendChild(titlesResult__itemResult);
+titlesResult.appendChild(titlesResult__itemAnswer1);
+titlesResult.appendChild(titlesResult__itemAnswer2);
+titlesResult.appendChild(titlesResult__itemAnswer3);
+titlesResult.appendChild(titlesResult__itemAnswer4);
+titlesResult.appendChild(titlesResult__itemAnswer5);
 
 result.classList.add("result");
-titles_result.classList.add("titles-result");
-titles_result__item_name.classList.add("titles-result__item");
-titles_result__item_lastname.classList.add("titles-result__item");
-titles_result__item_group.classList.add("titles-result__item");
-titles_result__item_result.classList.add("titles-result__item", "size-m");
-titles_result__item_answer1.classList.add("titles-result__item");
-titles_result__item_answer2.classList.add("titles-result__item");
-titles_result__item_answer3.classList.add("titles-result__item");
-titles_result__item_answer4.classList.add("titles-result__item");
-titles_result__item_answer5.classList.add("titles-result__item");
+titlesResult.classList.add("titles-result");
+titlesResult__itemName.classList.add("titles-result__item");
+titlesResult__itemLastname.classList.add("titles-result__item");
+titlesResult__itemGroup.classList.add("titles-result__item");
+titlesResult__itemResult.classList.add("titles-result__item", "size-m");
+titlesResult__itemAnswer1.classList.add("titles-result__item");
+titlesResult__itemAnswer2.classList.add("titles-result__item");
+titlesResult__itemAnswer3.classList.add("titles-result__item");
+titlesResult__itemAnswer4.classList.add("titles-result__item");
+titlesResult__itemAnswer5.classList.add("titles-result__item");
 
 $.addEventListener("submit", (e) => { e.preventDefault() });
 
-const IsWords = (str) => /^[a-zA-Zа-яА-Я]*$/.test(str);
-const IsNumeric = (str) => /^[0-9]*$/.test(str);
-const NotAnswer = (answer, convertor) => {
-    while (!(answer.trim())) {
-        alert("Вы не ввели ответ на вопрос");
+const isWords = (str) => /^[a-zA-Zа-яА-Я]*$/.test(str);
+const isNumeric = (str) => /^[0-9]*$/.test(str);
+const check = (answer, condition, convertor) => {
+    while (condition(answer) || !(answer.trim())) {
+        alert("Вы ввели некорректный ответ");
         answer = convertor();
     };
 };
 
-button.addEventListener("click", () => {
-    const name = input__item_name.value;
-    const lastname = input__item_lastname.value;
-    const group = input__item_group.value;
-
-    if (!(name).trim()) {
+const isGood = (name, lastname, group) => {
+    if (!name) {
         alert("Вы не внесли свое имя");
         return;
     };
-    if (!(lastname).trim()) {
+    if (!lastname) {
         alert("Вы не внесли свою фамилию");
         return;
     };
-    if (!(group).trim()) {
+    if (!group) {
         alert("Вы не внесли свою группу");
         return;
     };
-    if (!IsWords(name)) {
+    if (!isWords(name)) {
         alert("Имя не может содержать цифры и иные знаки");
-        $.getElementById("input_1").value = "";
         return;
     };
-    if (!IsWords(lastname)) {
+    if (!isWords(lastname)) {
         alert("Фамилия не может содержать цифры и иные знаки");
-        $.getElementById("input_2").value = "";
         return;
     };
+    return true;
+};
+
+button.addEventListener("click", () => {
+    const name = (input__itemName.value).trim();
+    const lastname = (input__itemLastname.value).trim();
+    const group = (input__itemGroup.value).trim();
+
+    const flag = isGood(name, lastname, group);
+    if (!flag) return;
 
     let answer1 = prompt('Сколько тебе лет?');
-    NotAnswer(answer1, () => answer1 = prompt('Сколько тебе лет?'));
-    while (!IsNumeric(answer1)) {
-        alert("Возраст не может содержать букв и символов");
-        answer1 = prompt('Сколько тебе лет?');
-    };
+    check(answer1, (ans) => !isNumeric(ans), () => answer1 = prompt('Сколько тебе лет?'));
 
     let answer2 = prompt('Из какого ты города?');
-    NotAnswer(answer2, () => answer2 = prompt('Из какого ты города?'));
-    while (!IsWords(answer2)) {
-        alert("Название города не может содержать цифр и символов");
-        answer2 = prompt('Из какого ты города?');
-    };
+    check(answer2, (ans) => !isWords(ans), () => answer2 = prompt('Из какого ты города?'));
 
     let answer3 = prompt('Ты lalala или okeoke?');
-    NotAnswer(answer3, () => answer3 = prompt('Ты lalala или okeoke?'));
-    while (!(answer3 == "lalala" || answer3 == "okeoke")) {
-        alert("Ответ может быть только lalala или okeoke");
-        answer3 = prompt('Ты lalala или okeoke?');
-    };
+    check(answer3, (ans) => !(ans == "lalala" || ans == "okeoke"), () => answer3 = prompt('Ты lalala или okeoke?'));
 
     let answer4 = prompt('Какой у тебя номер телефона?');
-    NotAnswer(answer4, () => answer4 = prompt('Какой у тебя номер телефона?'));
-    while (!IsNumeric(answer4)) {
-        alert("Номер телефона не может содержать букв и символов");
-        answer4 = prompt('Какой у тебя номер телефона?');
-    };
+    check(answer4, (ans) => !isNumeric(ans), () => answer4 = prompt('Какой у тебя номер телефона?'));
 
     let answer5 = prompt('Продолжи фразу: арбуз арбуз...');
-    NotAnswer(answer5, () => answer5 = prompt('Продолжи фразу: арбуз арбуз...'));
-    while (!IsWords(answer5)) {
-        alert("Эта фраза не может содержать цифр и символов");
-        answer5 = prompt('Продолжи фразу: арбуз арбуз...');
-    };
+    check(answer5, (ans) => !isWords(ans), () => answer5 = prompt('Продолжи фразу: арбуз арбуз...'));
 
-
-    titles_result__item_name.textContent = "Имя: " + name;
-    titles_result__item_lastname.textContent = "Фамилия: " + lastname;
-    titles_result__item_group.textContent = "Группа: " + group;
-    titles_result__item_result.textContent = "Результаты анкетирования:";
-    titles_result__item_answer1.textContent = "Возраст: " + answer1;
-    titles_result__item_answer2.textContent = "Город: " + answer2;
-    titles_result__item_answer3.textContent = "Lalala или okeoke: " + answer3
-    titles_result__item_answer4.textContent = "Телефон: " + answer4;
-    titles_result__item_answer5.textContent = "Арбуз арбуз: " + answer5;
+    titlesResult__itemName.textContent = "Имя: " + name;
+    titlesResult__itemLastname.textContent = "Фамилия: " + lastname;
+    titlesResult__itemGroup.textContent = "Группа: " + group;
+    titlesResult__itemResult.textContent = "Результаты анкетирования:";
+    titlesResult__itemAnswer1.textContent = "Возраст: " + answer1;
+    titlesResult__itemAnswer2.textContent = "Город: " + answer2;
+    titlesResult__itemAnswer3.textContent = "Lalala или okeoke: " + answer3
+    titlesResult__itemAnswer4.textContent = "Телефон: " + answer4;
+    titlesResult__itemAnswer5.textContent = "Арбуз арбуз: " + answer5;
 
     if (!$.querySelector('result_visible')) {
         result.classList.replace("result", "result_visible");
--- Pz11/Z6/Z6.js
+++ Pz11/Z6/Z6.js
@@ -7,35 +7,35 @@ const listQuestions = $.createElement("form");
 const header = $.createElement("header");
 const h2 = $.createElement("h2");
 const title = $.createElement("div");
-const label_input_title = $.createElement("label");
-const input_title = $.createElement("input");
+const labelInputTitle = $.createElement("label");
+const inputTitle = $.createElement("input");
 const p = $.createElement("p");
 const text = $.createElement("div");
 const question = $.createElement("div");
-const label_input_text = $.createElement("label");
-const input_text = $.createElement("input");
-const label_button = $.createElement("label");
+const labelInputText = $.createElement("label");
+const inputText = $.createElement("input");
+const labelButton = $.createElement("label");
 const button = $.createElement("button");
-const title_anket = $.createElement("div");
+const titleAnket = $.createElement("div");
 const list = $.createElement("ul");
 
 divform.appendChild(form);
 divform.appendChild(listQuestions);
 form.appendChild(header);
 form.appendChild(title);
-form.appendChild(label_input_title);
+form.appendChild(labelInputTitle);
 form.appendChild(p);
 form.appendChild(text);
 form.appendChild(question);
-listQuestions.appendChild(title_anket);
+listQuestions.appendChild(titleAnket);
 listQuestions.appendChild(list);
 header.appendChild(h2);
-label_input_title.appendChild(input_title);
-question.appendChild(label_input_text);
-question.appendChild(label_button);
-label_input_title.appendChild(input_title);
-label_input_text.appendChild(input_text);
-label_button.appendChild(button);
+labelInputTitle.appendChild(inputTitle);
+question.appendChild(labelInputText);
+question.appendChild(labelButton);
+labelInputTitle.appendChild(inputTitle);
+labelInputText.appendChild(inputText);
+labelButton.appendChild(button);
 
 $.body.appendChild(divform);
 
@@ -48,35 +48,38 @@ divform.id = "div_form";
 form.id = "form";
 header.classList.add("header");
 title.classList.add("title");
-input_title.classList.add("input");
-input_title.id = "input_title";
+inputTitle.classList.add("input");
+inputTitle.id = "inputTitle";
 text.classList.add("title");
 question.classList.add("question");
-input_text.classList.add("input");
-input_text.id = "input_text";
+inputText.classList.add("input");
+inputText.id = "inputText";
 button.classList.add("button");
 listQuestions.id = "form";
 listQuestions.classList.add("listQuestions");
-title_anket.classList.add("title", "size-m");
+titleAnket.classList.add("title", "size-m");
 list.classList.add("list");
 
 $.addEventListener("submit", (e) => { e.preventDefault() });
 
-let k = 0;
+let ind = 0;
 
+const isEmpty = (arg) => {
+    if (!arg) {
+        alert("Одно из полей ввода пустое")
+        return true;
+    }
+    return false;
+}
 button.addEventListener("click", () => {
-    if (!(input_title.value.trim())) {
-        alert("Вы не внесли название анкеты")
-        return;
-    };
-    if (!(input_text.value.trim())) {
-        alert("Вы не внесли текст вопроса")
-        return;
-    };
-    k++;
+    const title = inputTitle.value.trim();
+    const text = inputText.value.trim();
+
+    if (isEmpty(title) || isEmpty(text)) return;
+
     listQuestions.classList.replace("listQuestions", "listQuestions_visible")
     const quest = $.createElement("li");
     list.appendChild(quest);
-    title_anket.textContent = input_title.value;
-    quest.textContent = "Вопрос №" + k + '\r\n' + input_text.value;
+    titleAnket.textContent = title;
+    quest.textContent = "Вопрос №" + ++ind + '\r\n' + text;
 })
